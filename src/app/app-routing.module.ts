b3bf0dce027d057accd0ea46e8f5c212133bd929
lemytranepita <le-my.tran@epita.fr>
2023-12-05 00:45:36 +0100

    image-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ImageDetailsComponent } from './components/image-details/image-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: SearchBarComponent },
+  { path: 'image-details', component: ImageDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
